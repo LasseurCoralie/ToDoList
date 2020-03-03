4805c461548f69add6b7b8133e81caa45e7cfe10
LasseurCoralie <coralielasseur@gmail.com>
2020-03-03 13:56:53 +0100

    mise en place de linput dajout

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ const app = {
     init: function() {
         // vérification branchement js
         console.log('init');
+
+        // Ciblage élément id todo
+        app.todo = document.getElementById('todo');
+
         // Génération du form
         app.createForm();
         // Génération du compteur
@@ -12,10 +16,27 @@ const app = {
 
     createForm: function() {
         console.log('app : createForm');
+        // ajout du form
+        const form = document.createElement('form');
+        form.id = 'todo-form';
+
+        // ajout d'un input text et l'ajouter au form
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = 'todo-input';
+        input.autocomplete = 'off';
+        input.name = 'inputValue';
+        input.placeholder = 'Ajouter une tâche';
+
+        // ajout du form dans le dom
+        form.appendChild(input);
+        app.todo.appendChild(form);
     },
+
     createCounter: function() {
         console.log('app : createCounter');
     },
+
     createList: function() {
         console.log('app : createList');
     },
