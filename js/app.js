fc9c1866258a2fc299cf012bf8ef865f54bc5b16
LasseurCoralie <coralielasseur@gmail.com>
2020-03-03 13:34:55 +0100

    mis en place structure du base

--- js/app.js
+++ js/app.js
@@ -2,6 +2,22 @@ const app = {
     init: function() {
         // vérification branchement js
         console.log('init');
+        // Génération du form
+        app.createForm();
+        // Génération du compteur
+        app.createCounter();
+        // Génération de la liste
+        app.createList();
+    },
+
+    createForm: function() {
+        console.log('app : createForm');
+    },
+    createCounter: function() {
+        console.log('app : createCounter');
+    },
+    createList: function() {
+        console.log('app : createList');
     },
 };
 
