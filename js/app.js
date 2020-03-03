69d776263a286a680bdd075fa54654e261c1edf1
LasseurCoralie <coralielasseur@gmail.com>
2020-03-03 12:06:31 +0100

    branchement js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const app = {
+    init: function() {
+        // vérification branchement js
+        console.log('init');
+    },
+};
+
+// Chargement du DOM
+document.addEventListener('DOMContentLoaded', app.init)
